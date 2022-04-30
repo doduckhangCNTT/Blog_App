f36f6e58cc21f680f54b3ecaf2268a4b805e843c
Khang <doduckhang298@gmail.com>
2022-04-30 16:32:20 +0700

    delete command not use

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { showErrMsg } from "../../components/alert/Alert";
 import Loading from "../../components/alert/Loading";
